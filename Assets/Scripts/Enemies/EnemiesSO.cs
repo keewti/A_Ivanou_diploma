ea58c65139970318324720d7fd5eb2cf31d272a1
keewti <125391377+keewti@users.noreply.github.com>
2024-02-26 02:28:34 +0300

    backup

---
+++ Assets/Scripts/Enemies/EnemiesSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class EnemiesSO : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField] private int _hp;
+    [SerializeField] private int _dmg;
+    [SerializeField] private float _speed;
+
+    public string Name
+    { get { return _name; } }
+    public int HP
+    { get { return _hp; } }
+    public int DMG
+    { get { return _dmg; } }
+    public float Speed
+    { get { return _speed; } }
+}
