16588a9d12de3dbdc0b066543eb55df5742890de
keewti <125391377+keewti@users.noreply.github.com>
2024-03-10 22:04:51 +0300

    added smth idk
    menu with placeholder for audio, tilemap, maybe something else

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    public void Options()
+    {
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
